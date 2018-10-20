3f6b226137de31755e47867f167e82e5bea94e02
Nick Johnson <njohnson7@users.noreply.github.com>
2018-10-20 15:46:23 -0700

    fixed websockets
    Co-authored-by: YingCGooi <25574844+YingCGooi@users.noreply.github.com>
    Co-authored-by: Julius <Julius@Juliuss-MacBook-Pro.local>
    Co-authored-by: Nick Johnson <njohnson7@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
 # Shared Editor with Yjs prototype
-
-## Instructions
-1. Clone repo
-2. `npm install`
-3. `npm run compile`
-4. `npm start`
-5. Open two browser windows at http://localhost:12345 and type into textarea
-
-## Original Code
-https://github.com/ipfs-shipyard/shared-editing-demo
--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ require('y-memory')(Y)  // extend Y with the memory module
 require('y-websockets-client')(Y)
 require('y-array')(Y)
 require('y-text')(Y)
+require('y-map')(Y)
 
 Y({
   db: {
@@ -16,10 +17,14 @@ Y({
   share: {                         // specify the shared content
     array: 'Array',                // y.share.array is of type Y.Array
     text:  'Text',                 // y.share.text is of type Y.Text
+    map:   'Map',                  // y.share.map is of type Y.Map
   },
 }).then((y) => {                   // Yjs is successfully initialized
   window.y = y
+  window.Y = Y  // for debugging
   console.log('Yjs instance ready!')
   y.share.array                    // is a Y.Array instance
   y.share.text                     // is a Y.Text instance
+  y.share.map                      // is a Y.Map instance
+  y.share.text.bind(document.getElementById('textfield'))
 })
