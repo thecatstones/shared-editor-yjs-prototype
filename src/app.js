231a7b1fb4741594073a3da0471912fea717469e
Nick Johnson <njohnson7@users.noreply.github.com>
2018-10-21 18:14:12 -0700

    added custom websocket server
    Co-authored-by: YingCGooi <25574844+YingCGooi@users.noreply.github.com>
    Co-authored-by: Julius <Julius@Juliuss-MacBook-Pro.local>
    Co-authored-by: Nick Johnson <njohnson7@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ require('y-memory')(Y)  // extend Y with the memory module
 require('y-websockets-client')(Y)
 require('y-array')(Y)
 require('y-text')(Y)
-require('y-map')(Y)
 
 Y({
   db: {
@@ -12,21 +11,18 @@ Y({
   connector: {
     name: 'websockets-client',     // use the websockets connector
     room: 'my room',               // instances connected to the same room share data
-    // url: 'localhost:1234',      // specify your own server destination
+    url: 'https://catstones-websocket-server.herokuapp.com/',      // specify your own server destination
   },
   share: {                         // specify the shared content
     array: 'Array',                // y.share.array is of type Y.Array
     text:  'Text',                 // y.share.text is of type Y.Text
-    map:   'Map',                  // y.share.map is of type Y.Map
   },
 }).then((y) => {                   // Yjs is successfully initialized
   window.y = y
   window.Y = Y  // for debugging
   console.log('Yjs instance ready!')
-  y.share.array                    // is a Y.Array instance
-  y.share.text                     // is a Y.Text instance
-  y.share.map                      // is a Y.Map instance
 
+  // CodeMirror setup
   const code = document.getElementById('textfield')
   const editor = CodeMirror.fromTextArea(code, {
     mode:        'javascript',
@@ -34,7 +30,6 @@ Y({
     lineNumbers: true,
     tabSize:     2,
   })
-  window.X = { code, editor }  // for debugging
 
   y.share.text.bindCodeMirror(editor)
 })
