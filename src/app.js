bb3b5a7ca5b6defe176415a477f1eed92b6763bb
Nick Johnson <njohnson7@users.noreply.github.com>
2018-10-21 21:18:08 -0700

    added collaboration to console input and output
    Co-authored-by: YingCGooi <25574844+YingCGooi@users.noreply.github.com>
    Co-authored-by: Julius <Julius@Juliuss-MacBook-Pro.local>
    Co-authored-by: Nick Johnson <njohnson7@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ require('y-websockets-client')(Y)
 require('y-array')(Y)
 require('y-text')(Y)
 
-// let url = window.location.href.includes('heroku') ? 'https://catstones-websocket-server.herokuapp.com/' : undefined
+let url = window.location.href.includes('heroku') ? 'https://catstones-websocket-server.herokuapp.com/' : undefined
 
 Y({
   db: {
@@ -16,11 +16,13 @@ Y({
 
     // TODO: stop Chrome from blocking connection to heroku server when running locally
     // comment out url to use Yjs-provided WebSocket server
-    // url,
+    url,
   },
   share: {                         // specify the shared content
     array: 'Array',                // y.share.array is of type Y.Array
     text:  'Text',                 // y.share.text is of type Y.Text
+    text2:  'Text',
+    // text3:  'Text',
   },
 }).then((y) => {                   // Yjs is successfully initialized
   window.y = y
@@ -37,6 +39,44 @@ Y({
   })
 
   y.share.text.bindCodeMirror(editor)
+
+  // REPL
+  const consoleElm  = document.querySelector('#console')
+  const clearButton = document.querySelector('#clear')
+
+  clearButton.addEventListener('click', (event) => {
+    // consoleElm.innerHTML = ''
+    y.share.array.delete(0, y.share.array.length)
+  })
+
+  const consoleForm = document.querySelector('#console-form')
+  consoleForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let command = event.target.querySelector('#command').value
+    // consoleElm.innerHTML += `>> ${command}\n`
+    y.share.array.push([`>> ${command}\n`])
+    let value   = eval(command)
+    // let output  = (value === undefined ? 'undefined' : value) + '\n'
+    let output  = `${JSON.stringify(value)}\n`
+    // consoleElm.innerHTML += output
+    y.share.array.push([output])
+    
+    // consoleForm.reset()
+    y.share.text2.delete(0, y.share.text2.length)
+    // term.write(output)
+  })
+
+  y.share.text2.bind(document.querySelector('#command'))
+  // y.share.array.bind(consoleElm)
+  y.share.array.observe(event => {
+    if (event.type == 'insert') {
+      event.values.forEach(value => {
+        consoleElm.innerHTML += value
+      })
+    } else {
+      consoleElm.innerHTML = ''
+    }
+  })
 })
 
 // REPL
@@ -48,22 +88,3 @@ Y({
 // term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
 
 
-const consoleElm  = document.querySelector('#console')
-const clearButton = document.querySelector('#clear')
-
-clearButton.addEventListener('click', (event) => {
-	consoleElm.innerHTML = ''
-})
-
-const consoleForm = document.querySelector('#console-form')
-consoleForm.addEventListener('submit', (event) => {
-	event.preventDefault()
-  let command = event.target.querySelector('#command').value
-  consoleElm.innerHTML += `>> ${command}\n`
-  let value   = eval(command)
-  // let output  = (value === undefined ? 'undefined' : value) + '\n'
-  let output  = `${JSON.stringify(value)}\n`
-  consoleElm.innerHTML += output
-  consoleForm.reset()
-  // term.write(output)
-})
